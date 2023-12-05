abd04be77835309d369c3da6cd2882daef24bcc8
divinedibobie <57143668+divinedibobie@users.noreply.github.com>
2023-12-05 15:01:04 -0800

    Add files via upload

---
+++ cohort 8 fitness 2/src/index.js
@@ -0,0 +1,49 @@
+const change =document.getElementById("desc");
+
+change.addEventListener("click", (event)=> {
+  event.target.style.background="gainsboro"
+  event.target.style.color="white"
+}
+);
+const change2=document.getElementById("desc2");
+
+change2.addEventListener("click", (event)=> {
+  event.target.style.background="gainsboro"
+  event.target.style.color="white"
+}
+);
+const change3=document.getElementById("desc3");
+
+change3.addEventListener("click", (event)=> {
+  event.target.style.background="gainsboro"
+  event.target.style.color="white"
+}
+);
+const change4=document.getElementById("desc4");
+
+change4.addEventListener("click", (event)=> {
+  event.target.style.background="gainsboro"
+  event.target.style.color="white"
+}
+);
+const change5=document.getElementById("desc5");
+
+change5.addEventListener("click", (event)=> {
+  event.target.style.background="gainsboro"
+  event.target.style.color="white"
+}
+);
+const change6=document.getElementById("desc6");
+
+change6.addEventListener("click", (event)=> {
+  event.target.style.background="gainsboro"
+  event.target.style.color="white"
+}
+);
+const change7=document.getElementById("desc7");
+
+change7.addEventListener("click", (event)=> {
+  event.target.style.background="gainsboro"
+  event.target.style.color="white"
+}
+);
