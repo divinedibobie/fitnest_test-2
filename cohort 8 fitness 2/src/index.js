623b2791b2dd6bbfe9c1648c3004bc0847190ab9
divinedibobie <57143668+divinedibobie@users.noreply.github.com>
2023-12-07 04:02:13 -0800

    Add files via upload

--- cohort 8 fitness 2/src/index.js
+++ cohort 8 fitness 2/src/index.js
@@ -1,49 +1,92 @@
 const change =document.getElementById("desc");
 
 change.addEventListener("click", (event)=> {
-  event.target.style.background="gainsboro"
-  event.target.style.color="white"
+  event.target.style.background="black"
+  event.target.style.color="gainsboro"
 }
 );
 const change2=document.getElementById("desc2");
 
 change2.addEventListener("click", (event)=> {
-  event.target.style.background="gainsboro"
-  event.target.style.color="white"
+  event.target.style.background="black"
+  event.target.style.color="gainsboro"
 }
 );
 const change3=document.getElementById("desc3");
 
 change3.addEventListener("click", (event)=> {
-  event.target.style.background="gainsboro"
-  event.target.style.color="white"
+  event.target.style.background="black"
+  event.target.style.color="gainsboro"
 }
 );
 const change4=document.getElementById("desc4");
 
 change4.addEventListener("click", (event)=> {
-  event.target.style.background="gainsboro"
-  event.target.style.color="white"
+  event.target.style.background="black"
+  event.target.style.color="gainsboro"
 }
 );
 const change5=document.getElementById("desc5");
 
 change5.addEventListener("click", (event)=> {
-  event.target.style.background="gainsboro"
-  event.target.style.color="white"
+  event.target.style.background="black"
+  event.target.style.color="gainsboro"
 }
 );
 const change6=document.getElementById("desc6");
 
 change6.addEventListener("click", (event)=> {
-  event.target.style.background="gainsboro"
-  event.target.style.color="white"
+  event.target.style.background="black"
+  event.target.style.color="gainsboro"
 }
 );
 const change7=document.getElementById("desc7");
 
 change7.addEventListener("click", (event)=> {
-  event.target.style.background="gainsboro"
-  event.target.style.color="white"
+  event.target.style.background="black"
+  event.target.style.color="gainsboro"
 }
 );
+const change8=document.getElementById("desc8");
+
+change8.addEventListener("click", (event)=> {
+  event.target.style.background="black"
+  event.target.style.color="gainsboro"
+}
+);
+const change9=document.getElementById("desc9");
+
+change9.addEventListener("click", (event)=> {
+  event.target.style.background="black"
+  event.target.style.color="gainsboro"
+}
+);
+const change10=document.getElementById("desc10");
+
+change10.addEventListener("click", (event)=> {
+  event.target.style.background="black"
+  event.target.style.color="gainsboro"
+}
+);
+const change11=document.getElementById("desc11");
+
+change11.addEventListener("click", (event)=> {
+  event.target.style.background="black"
+  event.target.style.color="gainsboro"
+}
+);
+const change12=document.getElementById("desc12");
+
+change12.addEventListener("click", (event)=> {
+  event.target.style.background="black"
+  event.target.style.color="gainsboro"
+}
+);
+const change13=document.getElementById("desc13");
+
+change13.addEventListener("click", (event)=> {
+  event.target.style.background="black"
+  event.target.style.color="gainsboro"
+}
+);
+
